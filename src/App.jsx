b269b3c11ddbbcc2840b8901a975b71160e17834
DARKWA KELVIN OFORI <130385488+Thedarkwa@users.noreply.github.com>
2025-07-20 15:11:20 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import WelcomeMessage from './components/WelcomeMessage';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-function App() {
-    return (
-        <>
-            <WelcomeMessage />
-        </>
-    );
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
 }
 
- 
-export default App
+export default App;
+
