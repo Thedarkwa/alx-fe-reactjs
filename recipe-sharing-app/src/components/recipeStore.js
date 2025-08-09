bdc81e638b7638c8e25a6bfb1fd3391fd7b3ed38
DARKWA KELVIN OFORI <130385488+Thedarkwa@users.noreply.github.com>
2025-08-09 16:41:06 -0700

    Create recipeStore.js

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,16 @@
+// src/components/recipeStore.js
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
+  removeRecipe: (index) =>
+    set((state) => ({
+      recipes: state.recipes.filter((_, i) => i !== index),
+    })),
+}));
+
+export default useRecipeStore;
