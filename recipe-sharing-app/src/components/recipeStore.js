13286ab0b798821c1d31a45117c93e6e0a075ca5
DARKWA KELVIN OFORI <130385488+Thedarkwa@users.noreply.github.com>
2025-08-10 00:21:06 -0700

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,25 +1,48 @@
 // src/components/recipeStore.js
 import { create } from 'zustand';
 
-const useRecipeStore = create((set) => ({
+export const useRecipeStore = create((set, get) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
   
-  addRecipe: (recipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, { id: Date.now(), ...recipe }],
-    })),
+  setSearchTerm: (term) => {
+    set({ searchTerm: term });
+    get().filterRecipes();
+  },
 
-  updateRecipe: (id, updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
-      ),
-    })),
+  filterRecipes: () => {
+    const { recipes, searchTerm } = get();
+    set({
+      filteredRecipes: recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    });
+  },
 
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+  addRecipe: (newRecipe) => {
+    set((state) => {
+      const updated = [...state.recipes, newRecipe];
+      return { recipes: updated, filteredRecipes: updated };
+    });
+  },
+
+  updateRecipe: (updatedRecipe) => {
+    set((state) => {
+      const updated = state.recipes.map(recipe =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      );
+      return { recipes: updated, filteredRecipes: updated };
+    });
+  },
+
+  deleteRecipe: (id) => {
+    set((state) => {
+      const updated = state.recipes.filter(recipe => recipe.id !== id);
+      return { recipes: updated, filteredRecipes: updated };
+    });
+  },
+
+  setRecipes: (recipes) => set({ recipes, filteredRecipes: recipes })
 }));
 
-export default useRecipeStore;
