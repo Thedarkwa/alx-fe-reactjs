e5dbb635bd45cd71a3bb4ad7295c81abb2e0b207
DARKWA KELVIN OFORI <130385488+Thedarkwa@users.noreply.github.com>
2025-08-16 15:36:38 -0700

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,24 +1,10 @@
-
-import { useState } from "react";
-import SearchBar from "./components/SearchBar";
-import UserCard from "./components/UserCard";
-import { fetchGitHubUser } from "./services/githubApi";
+import Search from "./components/Search";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  const handleSearch = async (username) => {
-    const data = await fetchGitHubUser(username);
-    setUser(data);
-  };
-
   return (
-    <div className="min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-center p-6">
-        GitHub User Search
-      </h1>
-      <SearchBar onSearch={handleSearch} />
-      <UserCard user={user} />
+    <div>
+      <h1 style={{ textAlign: "center" }}>GitHub User Search</h1>
+      <Search />
     </div>
   );
 }
