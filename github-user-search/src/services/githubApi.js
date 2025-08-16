7952800159f38bf1560c8f5f2129e848dfaa0a2a
DARKWA KELVIN OFORI <130385488+Thedarkwa@users.noreply.github.com>
2025-08-16 15:20:29 -0700

    Update githubApi.js

--- github-user-search/src/services/githubApi.js
+++ github-user-search/src/services/githubApi.js
@@ -1,1 +1,15 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.github.com/users";
+
+// Function to search for a GitHub user
+export const fetchGitHubUser = async (username) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/${username}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return null;
+  }
+};
 
