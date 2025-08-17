0522512ba2e019573283113998ff374f3fad194d
DARKWA KELVIN OFORI <130385488+Thedarkwa@users.noreply.github.com>
2025-08-17 12:29:38 -0700

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
-  } catch (error) {
-    throw new Error("User not found");
-  }
+const BASE_URL = "https://api.github.com";
+
+// Advanced search using GitHub Search API
+export const fetchAdvancedUserSearch = async (username, location, minRepos) => {
+  let query = "";
+
+  if (username) query += `${username} `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos}`;
+
+  const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
+  return response.data;
 };
